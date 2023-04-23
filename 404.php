862d7f4a7062ce1316e66d2c6681289067314359
Jose <minonario23@gmail.com>
2023-04-23 10:26:22 -0400

    Se personaliza página 404 y se agrega status code 404 en response.

--- 404.php
+++ 404.php
@@ -20,6 +20,46 @@
 		<?php do_action( 'sinatra_before_content' ); ?>
 
 		<main id="content" class="site-content" role="main"<?php sinatra_schema_markup( 'main' ); ?>>
+                  
+                  <div class="home-top-visitas"> 
+                        <section id="topVisitas">
+                            <div class="title-section"> <span class="act">Lo más leído </span> </div>
+                            <?php 
+                              // args
+                              $args = array(
+                                  'post_type'        => 'post',
+                                  'post_status'      => 'publish',
+                                  'posts_per_page'   => 5,
+                                  'category__not_in' => 314,
+                                  'meta_key'         => 'post_views_count',
+                                  'orderby'          => 'meta_value_num',
+                                  'date_query' => array(
+                                                        array(
+                                                          'after' => '1 week ago'
+                                                        )
+                                                      ),
+                                  'order'            => 'DESC'
+                              );
+                              // query
+                              $the_query = new WP_Query( $args );
+
+                              if( $the_query->have_posts() ): ?>
+                                <div class="list-items">
+                                  <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                                    <article> 
+                                      <header>  
+                                        <h3 class="post-title"> 
+                                          <a href="<?php the_permalink(); ?>">
+                                            <?php the_title(); ?>
+                                          </a>
+                                        </h3> 
+                                      </header> 
+                                    </article>
+                              <?php endwhile; ?>
+                                </div>
+                              <?php endif; ?>
+                        </section>
+                    </div>
 
 			<?php do_action( 'sinatra_content_404' ); ?>
 
