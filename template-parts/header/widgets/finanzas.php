38cf2e0229ce90a5b431590e0365e2dd433bd4a4
Jose <minonario23@gmail.com>
2023-09-28 16:02:31 -0400

    Ajuste X social and others css

--- template-parts/header/widgets/finanzas.php
+++ template-parts/header/widgets/finanzas.php
@@ -12,6 +12,6 @@
 <div class="fdwidget">
 
   <div class="boleting-header"><a class="boton-suscribir" href="https://finanzasdigital.com/boletin" title="Suscríbete a nuestro boletín" >Boletín</a></div>
-  <div class="socials-sassy"><?php echo do_shortcode('[Sassy_Follow_Icons height="28" width="28" shape="round" social_networks="facebook=https://www.facebook.com/finanzasdigital,twitter=https://twitter.com/finanzasdigital,linkedin=https://www.linkedin.com/company/finanzas-digital,instagram=https://instagram.com/finanzasdigital"]') ?></div>
+  <div class="socials-sassy"><?php echo do_shortcode('[Sassy_Follow_Icons height="28" width="28" shape="round" social_networks="facebook=https://www.facebook.com/finanzasdigital,x=https://x.com/finanzasdigital,linkedin=https://www.linkedin.com/company/finanzas-digital,instagram=https://instagram.com/finanzasdigital"]') ?></div>
   
 </div>
